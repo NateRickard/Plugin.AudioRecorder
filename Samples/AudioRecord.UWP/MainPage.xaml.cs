20d5fb292d168bb4818741dc05461bd4bbe7b495
Nate Rickard <NateRickard@gmail.com>
2017-03-28 17:44:21 -0400

    added UWP sample app

---
+++ Samples/AudioRecord.UWP/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Plugin.AudioRecorder;
+using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace AudioRecord.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        AudioRecorderService recorder;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            recorder = new AudioRecorderService
+            {
+                StopRecordingOnSilence = false,
+                StopRecordingAfterTimeout = false
+            };
+
+            recorder.AudioInputReceived += Recorder_AudioInputReceived;
+        }
+
+        private async void Recorder_AudioInputReceived(object sender, string audioFile)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                recordBtn.Icon = new SymbolIcon(Symbol.Microphone);
+                recordBtn.Label = "Record";
+            });
+        }
+
+        public async Task PlayRecordedAudio(CoreDispatcher UiDispatcher)
+        {
+            MediaElement playback = new MediaElement();
+            StorageFolder temporaryFolder = ApplicationData.Current.TemporaryFolder;
+            var fileName = recorder.GetFilename();
+
+            await UiDispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                var storageFile = await StorageFile.GetFileFromPathAsync(fileName);
+
+                IRandomAccessStream stream = await storageFile.OpenAsync(FileAccessMode.Read);
+                playback.SetSource(stream, storageFile.FileType);
+                playback.Play();
+            });
+        }
+
+
+        private async void RecordBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!recorder.IsRecording)
+            {
+                recorder.StopRecordingOnSilence = checkTimeout.IsChecked.Value;
+
+                await recorder.StartRecording();
+                recordBtn.Icon = new SymbolIcon(Symbol.Stop);
+                recordBtn.Label = "Stop";
+            }
+            else
+            {
+                await recorder.StopRecording();
+                recordBtn.Icon = new SymbolIcon(Symbol.Microphone);
+                recordBtn.Label = "Record";
+            }
+        }
+
+
+        private async void PlayBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await PlayRecordedAudio(Dispatcher);
+        }
+    }
+}
