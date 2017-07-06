db1c47edc4d73eb591cce6e079cf2feaa1d8b552
Nate Rickard <nate.rickard@xamarin.com>
2017-07-06 14:27:56 -0400

    [ios] built ios sample

--- Samples/AudioRecord.iOS/ViewController.cs
+++ Samples/AudioRecord.iOS/ViewController.cs
@@ -1,25 +1,126 @@
-﻿using System;
-
+﻿using AVFoundation;
+using Foundation;
+using Plugin.AudioRecorder;
+using System;
+using System.Threading.Tasks;
 using UIKit;
 
 namespace AudioRecord.iOS
 {
     public partial class ViewController : UIViewController
     {
-        public ViewController(IntPtr handle) : base(handle)
+		AudioRecorderService recorder;
+		AVAudioPlayer player;
+
+		public ViewController(IntPtr handle) : base(handle)
         {
         }
+
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
-        }
+			// Perform any additional setup after loading the view, typically from a nib.
 
-        public override void DidReceiveMemoryWarning()
+			recorder = new AudioRecorderService
+			{
+				StopRecordingOnSilence = false,
+				StopRecordingAfterTimeout = false
+			};
+
+			recorder.AudioInputReceived += Recorder_AudioInputReceived;
+		}
+
+
+		public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
         }
-    }
+
+
+		async partial void RecordButton_TouchUpInside (UIButton sender)
+		{
+			await RecordAudio ();
+		}
+
+
+		async Task RecordAudio ()
+		{
+			try
+			{
+				if (!recorder.IsRecording)
+				{
+					recorder.StopRecordingOnSilence = TimeoutSwitch.On;
+
+					RecordButton.Enabled = false;
+
+					await recorder.StartRecording ();
+
+					RecordButton.SetTitle ("Stop", UIControlState.Normal);
+					RecordButton.Enabled = true;
+				}
+				else
+				{
+					RecordButton.Enabled = false;
+
+					await recorder.StopRecording ();
+
+					RecordButton.SetTitle ("Record", UIControlState.Normal);
+					RecordButton.Enabled = true;
+				}
+			}
+			catch (Exception ex)
+			{
+				//blow up the app!
+				throw ex;
+			}
+		}
+
+
+		private void Recorder_AudioInputReceived (object sender, string audioFile)
+		{
+			InvokeOnMainThread (() =>
+			{
+				RecordButton.SetTitle ("Record", UIControlState.Normal);
+
+				PlayButton.Enabled = !string.IsNullOrEmpty (audioFile);
+			});
+		}
+
+
+		partial void PlayButton_TouchUpInside (UIButton sender)
+		{
+			try
+			{
+				using (var session = AVAudioSession.SharedInstance ())
+				{
+					session.SetCategory (AVAudioSessionCategory.Ambient);
+					session.SetActive (true);
+
+					if (player != null)
+					{
+						player.Stop ();
+						player.Dispose ();
+					}
+
+					var uri = new NSUrl (recorder.GetFilename ());
+					NSError err;
+
+					player = new AVAudioPlayer (uri, "wav", out err);
+
+					if (err != null) throw new Exception (err.Description);
+
+					player.NumberOfLoops = 0;
+					player.Play ();
+					//player.FinishedPlaying += Player_FinishedPlaying;
+				}
+			}
+			catch (Exception ex)
+			{
+				//blow up the app!
+				throw ex;
+			}
+		}
+	}
 }
--- Samples/AudioRecord.iOS/ViewController.designer.cs
+++ Samples/AudioRecord.iOS/ViewController.designer.cs
@@ -1,17 +1,54 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace AudioRecord.iOS
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton PlayButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton RecordButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISwitch TimeoutSwitch { get; set; }
+
+        [Action ("PlayButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void PlayButton_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("RecordButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void RecordButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
         {
+            if (PlayButton != null) {
+                PlayButton.Dispose ();
+                PlayButton = null;
+            }
+
+            if (RecordButton != null) {
+                RecordButton.Dispose ();
+                RecordButton = null;
+            }
+
+            if (TimeoutSwitch != null) {
+                TimeoutSwitch.Dispose ();
+                TimeoutSwitch = null;
+            }
         }
     }
 }
